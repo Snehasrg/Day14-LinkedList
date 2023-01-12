b783427491c3f3decf9b0771d760a4aac94f97e3
Snehasrg <sghongade642@gmail.com>
2023-01-13 00:20:44 +0530

    Initializing Variables

---
+++ Nodes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Nodes
+    {
+          public int data;
+            public Nodes next;
+            public Nodes(int value)
+            {
+                this.data = value;
+            }
+        }
+}
