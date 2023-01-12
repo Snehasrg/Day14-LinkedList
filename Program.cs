f2c258919b0f2a37536a9996b481c241256aba38
Snehasrg <sghongade642@gmail.com>
2023-01-13 00:20:02 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Problem");
+
+            CreateLinkedList linkedList = new CreateLinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
