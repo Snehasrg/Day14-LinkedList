ba1aba19e75ee950fb6bbeac6fa62e2e59996ab5
Snehasrg <sghongade642@gmail.com>
2023-01-13 18:58:03 +0530

    Delete first Node

---
+++ DeleteFirst.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    public class DeleteFirst
+    {
+        private int size;
+        private Nodes head;
+
+        public DeleteFirst()
+        {
+            this.size = 0;
+            this.head = null;
+        }
+        public void insertNode(int data)
+        {
+            Nodes node = new Nodes();
+            node.data = data;
+            Nodes current = this.head;
+
+            if (this.head == null)
+            {
+                this.head = node;
+                this.head.next = null;
+                this.size = 1;
+                Console.WriteLine(this.head);
+            }
+            else
+            {
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = node;
+                node.next = null;
+                this.size += 1;
+            }
+        }
+
+        public void insertNth(int data, int position)
+        {
+            Nodes node = new Nodes();
+            node.data = data;
+            Nodes current = this.head;
+            if (this.head != null && position <= this.size)
+            {
+                for (int i = 1; i < position; i++)
+                {
+                    current = current.next;
+                }
+                node.next = current.next;
+                current.next = node;
+                this.size += 1;
+            }
+            else
+            {
+                Console.WriteLine("Exceeded the linked list size. Current Size: " + size);
+            }
+        }
+
+        public void deleteFirstNode()
+        {
+            if (head != null)
+            {
+                this.head = this.head.next;
+                this.size--;
+            }
+            else
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+        }
+
+
+        public void printNodes()
+        {
+            if (this.size < 1)
+                Console.WriteLine("There are no nodes in the linked list");
+
+            else
+            {
+                Nodes current = this.head;
+                for (int i = 0; i < this.size; i++)
+                {
+                    Console.WriteLine("Node " + current.data + " is at location " + i);
+                    current = current.next;
+                }
+            }
+        }
+
+
+        public int getListSize()
+        {
+            return size;
+        }
+    }
+}
