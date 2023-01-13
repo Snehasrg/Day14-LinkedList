d0bbbdd584d8ef0c3a6baa3d876bb662bf596316
Snehasrg <sghongade642@gmail.com>
2023-01-13 16:59:44 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
-﻿namespace LinkedList
+﻿using System.Collections.Generic;
+
+namespace LinkedList
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Linked List Problem");
+            Insert insert= new Insert();
 
-            AddingLinkedList list = new AddingLinkedList();
+            Console.WriteLine("Current Size of the list is: " + insert.getListSize());
+            insert.insertNode(56);
 
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
-            list.insertNode(56);
-            list.insertNode(30);
-            list.insertNode(70);
-            list.printNodes();
+            insert.insertNode(70);
+            insert.printNodes();
+
+            Console.WriteLine("Adding 30 at 1st Location");
+            insert.insertNth(30, 1);
+            insert.printNodes();
         }
     }
 }
