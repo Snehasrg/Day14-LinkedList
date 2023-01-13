33ca270971f9d4a79c477383373f8558688eca2e
Snehasrg <sghongade642@gmail.com>
2023-01-13 20:31:22 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,21 +7,16 @@ namespace LinkedList
         public static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Linked List Problem");
-            Delete40 list = new Delete40();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            OrderdLinkedList list = new OrderdLinkedList();
 
             list.insertNode(56);
             list.insertNode(30);
             list.insertNode(40);
             list.insertNode(70);
             list.printNodes();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
-            Console.WriteLine("-------------");
-            Console.WriteLine("Deleting node from 2nd location");
-            list.deleteNthNode(2);
-            list.printNodes();
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            Console.WriteLine();
+            list.orderNodes();
 
         }
     }
