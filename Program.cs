68c7ef1e15803b18ba578c005e39d0f6bd9f9b88
Snehasrg <sghongade642@gmail.com>
2023-01-13 16:51:53 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@
             //Console.WriteLine("Welcome to Linked List Problem");
 
             AddingLinkedList list = new AddingLinkedList();
+
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            list.insertNode(56);
             list.insertNode(30);
             list.insertNode(70);
-            list.insertNode(56);
             list.printNodes();
         }
     }
